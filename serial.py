cacd90eb23ff0729fdc76a4ac31b9988eed7e7b2
oldhill <oldhillcode@gmail.com>
2016-03-20 16:57:16 -0700

    Add serial calculation of sum of factorials

---
+++ serial.py
@@ -0,0 +1,20 @@
+# Serial sum of factorials
+
+import math
+
+INPUT_FILE_COUNT = 10
+INPUT_FILE_DIR = 'data/'
+OUTPUT_FILE = 'results/serial.txt'
+
+for i in xrange(0, INPUT_FILE_COUNT):
+  file_name = '%s%s.txt' % (INPUT_FILE_DIR, i)
+  print 'Reading and processing file %s' % file_name
+  total = 0
+  with open(file_name, 'r') as infile:
+    for l in infile:
+      total += math.factorial(int(l))
+
+with open(OUTPUT_FILE, 'w') as outfile:
+  outfile.write('%s\n' % total)
+
+print 'Done'
